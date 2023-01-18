ffcce48bba3d654083fd5204719dc069f5eca52f
manali1321 <patelmanali216@gmail.com>
2023-01-17 21:43:01 -0500

    Node.js Express.Js and Pug lab-2

---
+++ index.js
@@ -0,0 +1,33 @@
+const { request } = require("express");
+
+//  Import Required modules
+const express = require("express");
+const path = require("path");
+
+// set up express app and port number
+const app = express();
+const port = 8888;
+
+// set up template engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// set up static file path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'Image')));
+
+// page routes
+app.get('/', (request, response) => {
+    // response.status(200).send('test Page 1');
+    response.render('index', { title: 'Home' });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about', { title: 'About' });
+});
+
+// set up server listening
+app.listen(port, () => {
+    console.log(`listening on http://localhost:${port}`);
+});
